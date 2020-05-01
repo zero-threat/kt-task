5e7ff0669518afafa5fb664d18a515af20930184
stas <voznyuk_stas@mail.ru>
2020-05-01 11:27:37 +0300

    (fix)-taskedit: fixed adaptive

--- src/components/TaskEdit.vue
+++ src/components/TaskEdit.vue
@@ -32,5 +32,19 @@
 </script>
 
 <style lang="scss" scoped>
+@media screen and (max-width: 700px) {
+    input {
+        font-size: 16px;
+    }
+    
+    .def-form {
+        width: 60%;
+    }
+}
 
+@media screen and (min-width: 320px) and (max-width: 600px) {
+    input {
+        font-size: 14px;
+    }
+}
 </style>
